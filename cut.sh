d89b9d0fb5d92534daa0c1751cd0a83c4e7954a6
misumi-cr <misumi@criepi.denken.or.jp>
2022-12-23 20:56:07 +0900

    グローバル変数考慮、コピーコマンド追加

--- cut.sh
+++ cut.sh
@@ -1,46 +1,56 @@
 #!/bin/sh -x
 
 function cut_data (){
-# $1: file type (a, h, r)
-# $2: file number (nnumX)
-# $3: cut start
-# $4: cut end
-  if [ ! $# -eq 4 ]; then
+  # $1: file type (a, h, r)
+  # $2: cut start
+  # $3: cut end
+  if [ ! $# -eq 3 ]; then
     echo "[${0}] Four arguments must be specified."
     exit
   fi
   echo "processing $1 file"
-  mv -f $cname.$cnum.${1}.${2}.nc $cname.$cnum.${1}_cut_data.${2}.nc
-  ncks -d ocean_time,${3},${4} $cname.$cnum.${1}_cut_data.${2}.nc -o $cname.$cnum.${1}.${2}.nc
+  mv -f $cname.$cnum.${1}.${nnum0}.nc $cname.$cnum.${1}_cut_data.${nnum0}.nc
+  ncks -d ocean_time,${2},${3} $cname.$cnum.${1}_cut_data.${nnum0}.nc -o $cname.$cnum.${1}.${nnum0}.nc
 }
 
 function join_data_2 (){
-# $1: file type (a, h, r)
-# $2: file number (nnum0)
-  if [ ! $# -eq 2 ]; then
-    echo "[${0}] Two arguments must be specified."
+  # $1: file type (a, h, r)
+  if [ ! $# -eq 1 ]; then
+    echo "[${0}] One argument must be specified."
     exit
   fi
   echo $cname
   echo $cnum
   n=$(expr $2 + 1)
   nnum1=$(printf "%03d\n" "${n}")
-  mv -f $cname.$cnum.${1}.${2}.nc $cname.$cnum.${1}_join_data_2.${2}.nc
-  ncrcat $cname.$cnum.${1}_join_data_2.${2}.nc $cname.$cnum.${1}.$nnum1.nc -o $cname.$cnum.${1}.${2}.nc
+  mv -f $cname.$cnum.${1}.${nnum0}.nc $cname.$cnum.${1}_join_data_2.${nnum0}.nc
+  ncrcat $cname.$cnum.${1}_join_data_2.${nnum0}.nc $cname.$cnum.${1}.$nnum1.nc -o $cname.$cnum.${1}.${nnum0}.nc
+}
+
+function copy_data (){
+  # $1 file type (a, h, r)
+  if [ ! $# -eq 1 ]; then
+    echo "[${0}] One argument must be specified."
+    exit
+  fi
+  cp ${cname}.${cnum}.${1}.${nnum0}.nc ../out/
 }
 
 ### main ###
 
 cname="h010_l045_v001.190515"
 cnum="101"
+nnum0=070
 
-### 070+071 -> 070 step1
-#nnum0=070
-#cut_data a $nnum0 0 2
-#cut_data h $nnum0 0 2
-#cut_data r $nnum0 0 0
+### 070+071 -> 070
+#cut_data a 0 2
+#cut_data h 0 2
+#cut_data r 0 0
 
-### 070+071 -> 070 step2
-#join_data_2 a $nnum0
-#join_data_2 h $nnum0
-#cut_data r $nnum0 0 0
+#join_data_2 a
+#join_data_2 h
+#cut_data r 0 0
+
+#copy_data a
+#copy_data h
+#copy_data r
