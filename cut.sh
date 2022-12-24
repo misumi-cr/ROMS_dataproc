3165295ec04fd6ecdf1f663446d2c3ca684d1a4a
misumi-cr <misumi@criepi.denken.or.jp>
2022-12-24 12:35:35 +0900

    テストコード

--- cut.sh
+++ cut.sh
@@ -27,6 +27,15 @@ function join_data_2 (){
   ncrcat bak/$cname.$cnum.${1}.${nnum0}.nc $cname.$cnum.${1}.${nnum1}.nc -o $cname.$cnum.${1}.${nnum0}.nc
 }
 
+function rewind_num (){
+  # $1 file type (a, h, r)
+  if [ ! $# -eq 1 ]; then
+    echo "[${0}] One argument must be specified."
+  fi
+  cp ${cname}.${cnum}.r.${nnum0}.nc bak/${cname}.${cnum}.r.${nnum0}.nc 
+  cp ${cname}.${cnum}.r.${nnum1}.nc ${cname}.${cnum}.r.${nnum0}.nc 
+}
+
 function copy_data (){
   # $1 file type (a, h, r)
   if [ ! $# -eq 1 ]; then
@@ -42,20 +51,29 @@ cname="h010_l045_v001.190515"
 cnum="101"
 nnum0=076
 
-mkdir -p bak
+mkdir -p org
 
-n=$(expr ${nnum0} + 1)
-nnum1=$(printf "%03d\n" "${n}")
+set a r w
+while [ $# -ge 1 ]
+do
+  echo $1
+  shift
+done
 
-### 076+077 -> 076
-#cut_data a $nnum0 0 2
-#cut_data h $nnum0 0 2
-#cut_data r $nnum0 0 0
-
-#join_data_2 a
-#join_data_2 h
-#cut_data r $nnum1 0 0
-
-#copy_data a
-#copy_data h
-#copy_data r
+#n=$(expr ${nnum0} + 1)
+#nnum1=$(printf "%03d\n" "${n}")
+#
+#### 076+077 -> 076
+##cut_data a $nnum0 0 2
+##cut_data h $nnum0 0 2
+##cut_data r $nnum0 0 0
+#
+##join_data_2 a
+##join_data_2 h
+##cut_data r $nnum1 0 0
+#mv ${cname}.${cnum}.r.${nnum1}.nc ${cname}.${cnum}.r.${nnum0}.nc 
+#
+#
+##copy_data a
+##copy_data h
+##copy_data r
